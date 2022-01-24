52143c24880560b8f2fa69e46cd073d28c5bbc97
Liviu Balan <liviu.balan@emag.ro>
2022-01-24 20:30:25 +0200

    Reformat code for "CITY_DATA" variable
    It seems that on some OS we encounter the following problem:
    Please note that the code you submit should run without any error. Your code produces a "SyntaxError" exception when you execute the following line
     -->    }

--- bikeshare.py
+++ bikeshare.py
@@ -3,11 +3,9 @@ import pandas as pd
 import sys
 import time
 
-CITY_DATA = {
-    'chicago': 'chicago.csv',
-    'new york city': 'new_york_city.csv',
-    'washington': 'washington.csv'
-}
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
 ALL = 'all'
 
 
